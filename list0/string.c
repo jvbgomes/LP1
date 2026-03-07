038a2946258437a3791ca60211e124e9f31bda08
jvbgomes <jvbg16@gmail.com>
2026-03-07 17:16:04 -0300

    qst da lista 0

--- list0/string.c
+++ list0/string.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int main() {
+int main(void) {
+    char str[100] = {0};
+    size_t qtd = 0;
+    size_t i;
+
+    printf("Digite uma string: ");
+    if (fgets(str, sizeof(str), stdin) != NULL) { // le a string, garantindo que nao passe o limite do buffer e verifica se a leitura foi bem-sucedida.
+        str[strcspn(str, "\n")] = '\0'; // Remove o caractere de nova linha, se presente.
+
+        for (i = 0; str[i] != '\0'; i++) { // varre a string ate o caractere nulo.
+            if (!isspace((unsigned char)str[i])) { //Verifica se o caractere é um blank space.
+                qtd++;
+            }
+        }
+
+        printf("A string digitada foi: %s\n", str);
+        printf("Quantidade de caracteres: %zu\n", qtd);
+    }
 
     return 0;
 }
