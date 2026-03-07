2bfbae34c4f902a5a9043041e1e0fff6c532419e
jvbgomes <jvbg16@gmail.com>
2026-03-07 17:26:37 -0300

    verificao de primo

--- list0/verifprime.c
+++ list0/verifprime.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main() {
+    int n;
+    int primo = 1;
+
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        primo = 0;
+    } else {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                primo = 0;
+                break;
+            }
+        }
+    }
+
+    if (primo) {
+        printf("%d eh primo\n", n);
+    } else {
+        printf("%d nao eh primo\n", n);
+    }
 
     return 0;
 }
