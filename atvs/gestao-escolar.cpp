cf944fd86bce209af8c2ab134606eb3c7453d83c
João Victor <jvbg16@gmail.com>
2026-03-28 04:54:02 -0300

    atv de LP1(com erro da saida cin >> opcao)

--- atvs/gestao-escolar.cpp
+++ atvs/gestao-escolar.cpp
@@ -22,8 +22,59 @@ public:
     class Data nascimento;
 
     void imprimirAluno() {
-        cout << "Matrícula: " << matricula << " | Nome: " << nome << " | Nasc: " << ;
-        imprimirData(nascimento);
+        cout << "Matricula: " << matricula << " | Nome: " << nome << " | Nasc: " ;
+        nascimento.imprimirData();
         cout << "\n";
     }
+};
+
+int main() {
+    Aluno turma[MAX_ALUNOS];
+    int totalAlunos = 0;
+    int opcao = -1;
+
+    cout << "=== Sistema de Gestao Escolar ===" << "\n" ;
+
+    while (opcao != 0) {
+        cout << "\n" << "Menu" << "\n" ;
+        cout << "1. Adicionar Aluno" << "\n" ;
+        cout << "2. Listar Alunos" << "\n" ;
+        cout << "0. Sair" << "\n" ;
+        cout << "Escolha: " ;
+        cin >> opcao;
+
+        if (opcao == 1) {
+            if (totalAlunos < MAX_ALUNOS) {
+                cout << "Digite a matricula: " ;
+                cin >> turma[totalAlunos].matricula;
+
+                cout << "Digite o primeiro nome do aluno: " ;
+                cin >> turma[totalAlunos].nome;
+
+                cout << "Data de nascimento(dia): " ;
+                cin >> turma[totalAlunos].nascimento.dia;
+
+                cout << "Data de nascimento(mes): " ;
+                cin >> turma[totalAlunos].nascimento.mes;
+
+                cout << "Data de nascimento(ano): " ;
+                cin >> turma[totalAlunos].nascimento.ano;
+
+                totalAlunos++;
+                cout << "Aluno adicionado com sucesso!" << "\n" ;
+            } else {
+                cout << "Capacidade maxima da turma foi atingida!" << "\n" ;
+            }
+
+        } else if (opcao == 2) {
+            cout << "\n" << "--- Lista de Alunos ---" << "\n" ;
+            for (int i = 0; i < totalAlunos; i++) {
+                turma[i].imprimirAluno();
+            }
+        } else if (opcao != 0) {
+            cout << "Opcao invalida" << "\n" ;
+        }
+    }
+
+    return 0;
 }
