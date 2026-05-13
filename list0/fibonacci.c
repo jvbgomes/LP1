0e067709cc3bf0bad2e9cdc1aba04d0c315701fa
jvbgomes <jvbg16@gmail.com>
2026-05-12 23:20:53 -0300

    add fibonacci sequence with recursive implementation

---
+++ list0/fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    printf("Quantos termos da sequencia de Fibonacci? ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Numero invalido.\n");
+        return 1;
+    }
+
+    printf("Fibonacci(%d): ", n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+
+    return 0;
+}
