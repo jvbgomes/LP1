58fccc764de455c3ffe0a24794fc51aa617a1314
jvbgomes <jvbg16@gmail.com>
2026-05-12 23:26:04 -0300

    refactor factorial: extract to function and handle negative input

--- list0/factorial.c
+++ list0/factorial.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
+int factorial(int n) {
+    if (n < 0) return -1;
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
 int main() {
-    int num, factorial;
+    int num;
     printf("Digite um numero: ");
     scanf("%d", &num);
 
-    factorial = 1;
-    for(int i = 1; i <= num; i++) {
-        factorial *= i;
+    if (num < 0) {
+        printf("Fatorial nao definido para numeros negativos.\n");
+        return 1;
     }
-    printf(" %d! = %d", num, factorial);
+
+    printf("%d! = %d\n", num, factorial(num));
     return 0;
 }
